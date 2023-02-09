cafb657f4375f3a3156c91fd7b5feb8469592be3
TalibMalik7 <mohammedtalib5@gmail.com>
2023-02-09 13:39:53 +0530

    first

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+  public  static  void main(String[] args){
+      RWOnly rw = new RWOnly();
+    //  rw.name = "Tal";
+    //  System.out.println(rw.name);  //name has private access in com.driver.RWOnly
+      rw.setName("Tal");
+      System.out.println(rw.getName());
+  }
 }
